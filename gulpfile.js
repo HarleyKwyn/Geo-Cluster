77bbf3520d331dd581e19296c4022ede8740ddda
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-13 14:36:23 -0700

    Updates gulp file for new directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,13 @@ gulp.task('develop', function () {
   process.env.NODE_ENV = 'dev';
   nodemon({ script: 'server.js', ext: 'styl html js', ignore: ['node_modules/', 'test/'] })
     .on('change', ['stylus', 'lint'])
+    .on('restart', function(){
+      console.log('restarted');
+    }) 
 });
 
 gulp.task('lint', function(){
-  gulp.src('./scripts/*')
+  gulp.src('./public/scripts/**/*.js')
   .pipe(lint())
   .pipe(lint.reporter('default'));
 });
