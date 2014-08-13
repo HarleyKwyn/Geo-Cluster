5a266a2fb568d11aa7c59a7d28e88f782fc4a8b9
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-13 12:46:22 -0700

    Impliments mock user data service

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,26 @@
 angular.module( 'GeoCluster', [
   'ui.bootstrap',
   'd3',
-   'GeoClusterGraph'
+  'UserData',
+  'GeoClusterGraph'
 ])
+
+.constant('url', 'http://172.12.8.150/')
 
 .controller('MainController', [
   '$scope',
-  function($scope){
+  'userData',
+  function($scope, UserData){
+    UserData.success(function(data){
+      $scope.userData = data;
+      console.log(data);
+    });
     $scope.data = [
       {name: "test1", score: 98},
       {name: "test2", score: 96},
       {name: 'test3', score: 75},
       {name: "test4", score: 48}
-    ];;
+    ];
 }])
 
 ;
