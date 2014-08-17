2f573f013c0d38c5886c42b28bad47a5ce6abdde
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-17 13:26:46 -0700

    Adds test for initMeans

--- test/cluster-data.spec.js
+++ test/cluster-data.spec.js
@@ -1,29 +1,32 @@
 describe('Module: cluster-data', function(){
+  
   var kmeans;
   var injector = angular.injector(['cluster-data', 'data-processing']);
       attachCoords = injector.get('attachCoords')
-  attachCoords(kmeansData);
-  attachCoords(testData);
+  
+  before(function(){
+    attachCoords(kmeansData);
+    attachCoords(testData);
+  });
 
   describe('factory: kmeans', function(){
+
     beforeEach(function(){
       kmeans = injector.get('kmeans');
       testKmeans = new kmeans(4, kmeansData, 'coords')
       smallKmeans = new kmeans(2, testData, 'coords')
-    });
-    it('should return an function', function(){
       console.log(testKmeans);
       console.log(smallKmeans);
+    });
+
+    it('should return an function', function(){
       expect( typeof kmeans ).to.equal('function');
     });
+
     describe('.getDataExtremes', function(){
       it('should return and array of length 2', function(){
         expect( Array.isArray( testKmeans.getDataExtremes() ) ).to.be.true;
         expect( testKmeans.getDataExtremes().length ).to.equal(2);
-      });
-      it('should store getDataExtremes result in kmeans.extremes', function(){
-        var extremes = testKmeans.getDataExtremes();
-        expect( testKmeans.extremes ).to.deep.equal(extremes);
       });
       it('should have latitude min and max', function(){
         expect(testKmeans.extremes[0].min).to.be.defined
@@ -38,12 +41,35 @@ describe('Module: cluster-data', function(){
         expect(smallKmeans.extremes).to.deep.equal(correct);
       });
     });
+
     describe('.getDataRanges', function(){
       it('should calculate ranges correctly', function(){
         var correct = smallKmeans.extremes[0].max - smallKmeans.extremes[0].min
         expect(smallKmeans.ranges[0]).to.be.equal(correct);
       });
     });
+    describe('.initMeans', function(){
+      it('should return and array of length 4 with sub Arrays of length 2', function(){
+        var result = testKmeans.initMeans() 
+        console.log(result);
+        expect( Array.isArray( result ) ).to.be.true;
+        expect( result.length ).to.equal(4);
+        for (var i = result.length - 1; i >= 0; i--) {
+          expect( result[i].length ).to.equal(2);
+        }
+      });
+      it('should generate random starting mean dimensions', function(){
+        var first = testKmeans.initMeans();
+        var second = testKmeans.initMeans();
+        expect(first).to.not.deep.equal(second);
+      });
+    });
+    describe('.calculate', function(){
+      xit('should store getDataExtremes result in kmeans.extremes', function(){
+        var extremes = testKmeans.getDataExtremes();
+        expect( testKmeans.extremes ).to.deep.equal(extremes);
+      });  
+    })
 
   });
 });
