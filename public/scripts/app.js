ec4d987534922ff2cc5ff8661b69bf0b5b887c7c
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-18 14:50:06 -0700

    Updates controller with hover function and new models

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,21 +15,49 @@ angular.module( 'GeoCluster', [
   'attachCoords',
   'kmeans',
   function($scope, getData, dataFilter, attachCoords, kmeans){
-    var origonalData  = null;
+    var filter,
+        origonalData  = null;
+
     $scope.data = null;
-    $scope.zoom = 1;
+    $scope.currentCluster = {data:null, index:null};
+    $scope.kmeans = null;
+    $scope.error = null;
     $scope.timeRange = {min:0, max:0};
     $scope.k = 4;
-    $scope.kmeans = null;
+    $scope.tolerance = 100;
+    $scope.zoom = 1;
+    $scope.clusters = null;
 
     getData.success(function(data){
       origonalData = attachCoords(data);
       $scope.data = origonalData;
-      $scope.kmeans = new kmeans($scope.data, $scope.k);
+      $scope.kmeans = new kmeans($scope.data, $scope.k, $scope.tolerance);
       $scope.clusters = $scope.kmeans.clusters;
       $scope.centroids = $scope.kmeans.means;
+      filter = new dataFilter($scope.data);
       console.log($scope.centroids)
     });
+
+    $scope.showCluster = function(d, i){
+      $scope.currentCluster.index = i+1;
+      $scope.currentCluster.data = $scope.clusters[i];
+      $scope.$apply();
+    };
+
+    $scope.filter = function(){
+      console.log("filtered")
+      filter.byModule($scope.module);
+      console.log(filter);
+      if(filter.filteredData.length === 0 ){
+        $scope.error = "No Matches"
+      }else{
+        $scope.error = null;
+        $scope.data = filter.filteredData;
+        $scope.kmeans.recalculate( filter.filteredData );
+        $scope.clusters = $scope.kmeans.clusters;
+        $scope.centroids = $scope.kmeans.means;
+      }
+    }
 
 }])
 
