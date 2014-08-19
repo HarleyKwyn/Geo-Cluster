75161f3c097bb1fad99df773cdd05545e06c1e9e
HarleyKwyn <kwyn.meagher@gmail.com>
2014-08-18 17:17:01 -0700

    Removes process environemnt variable in server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var publicDir = process.env.NODE_ENV === 'dev' ? '/public/' : '/dist/';
+var publicDir = '/public/';
 
 app.use('/data/:fileName', function(req,res){
   var file = req.params.fileName
